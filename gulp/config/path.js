a5e99494fa6bfcc3bbcf629fcfaeac11b561356a
arsen <arsen.yeghiazarian@gmail.com>
2022-03-15 12:23:19 +0400

    unused file delete, stuctural fixes

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,23 +10,23 @@ export const path = {
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
         files: `${buildFolder}/files`,
-        images: `${buildFolder}/img`,        
+        images: `${buildFolder}/images`,        
     },
     src: {
-        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp}`,
-        svg: `${srcFolder}/img/**/*.svg`,
+        images: `${srcFolder}/assets/images/**/*.{jpg, jpeg, png, gif, webp}`,
+        svg: `${srcFolder}/assets/images/**/*.svg`,
         js: `${srcFolder}/js/app.js`,
         scss: `${srcFolder}/scss/main.scss`,
         html: `./*.html`,
         files: `${srcFolder}/files/**/*.*`,
-        svgIcons: `${srcFolder}/img/svgicons/*svg`
+        svgIcons: `${srcFolder}/assets/images/svgicons/*svg`
     },
     watch: {
         js: `${srcFolder}/js/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
         files: `${srcFolder}/files/**/*.*`,
         html: `${srcFolder}/**/*.html`,
-        images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp, svg}`,
+        images: `${srcFolder}/images/**/*.{jpg, jpeg, png, gif, webp, svg}`,
     },
     clean: buildFolder,
     buildFolder: buildFolder,
